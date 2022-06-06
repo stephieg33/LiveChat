c1054c119880f048cfed7dd9a77d9d7fa698d67a
Stephen Gash <stephieg33@gmail.com>
2022-06-06 14:51:07 +0000

    Add user and message models and database tables

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Message.create(body: "Welcome to the chat!", user_id: "1")
+Message.create(body: "Another message", user_id: "1")
+Message.create(body: "Cool this thing is great", user_id: "1")
+Message.create(body: "Let's build something amazing", user_id: "1")
